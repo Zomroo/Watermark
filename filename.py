7f909f0d1d829245448e5a8e9fc5c3500d3304a5
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-04 23:54:21 +0530

    Update filename.py

--- filename.py
+++ filename.py
@@ -1,47 +1,52 @@
-import pyrogram
 import os
-import cv2
-from pyrogram import filters
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+# Create a Pyrogram client instance
+app = Client("my_bot")
 
 
-app = pyrogram.Client(
-    "my_bot",
-    api_id=15849735,  # Replace with your own Telegram API ID
-    api_hash="b8105dc4c17419dfd4165ecf1d0bc100",  # Replace with your own Telegram API hash
-    bot_token="6067171502:AAF20GwzoxblYC4yBNw8nTffRJprwgLTKi0"  # Replace with your own bot token
-)
+# Filter for video messages
+video_filter = filters.video & ~filters.edited
 
-@app.on_message(filters.command(["start"]))
-def start(bot, update):
-    bot.send_message(chat_id=update.chat.id, text='This bot can add a watermark to your videos. Use the /set command to set the watermark.')
 
-@app.on_message(filters.command(["set"]))
-def set_watermark(bot, update):
-    message = update.reply_to_message
-    if message.video:
-        video_path = message.download(file_name="temp_video.mp4")
-        watermark_text = "@OnlyFanstash"
-        video = cv2.VideoCapture(video_path)
-        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# Function to set watermark on video
+def set_watermark(video_path):
+    # Get the filename and extension
+    filename, extension = os.path.splitext(video_path)
 
-        while True:
-            ret, frame = video.read()
-            if not ret:
-                break
-            cv2.putText(frame, watermark_text, (width-200, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-            cv2.imwrite('temp_frame.jpg', frame)
-            os.system(f'ffmpeg -y -i temp_frame.jpg -i "{video_path}" -map 0:0 -map 1:1 -c:v libx264 -preset ultrafast -crf 22 -c:a copy -shortest temp_output.mp4')
-            os.remove('temp_frame.jpg')
-            os.remove(video_path)
-            if os.path.exists('temp_output.mp4'):
-                os.rename('temp_output.mp4', video_path)
+    # Set the path for the output file
+    output_path = f"{filename}_watermarked{extension}"
 
-        video = open(video_path, 'rb')
-        update.reply_video(video)
-        video.close()
-        os.remove(video_path)
-    else:
-        bot.send_message(chat_id=update.chat.id, text="Please reply to a video to add watermark.")
+    # Set the command to add the watermark
+    command = f"ffmpeg -i {video_path} -i watermark.png -filter_complex 'overlay=10:10' {output_path}"
 
+    # Run the command
+    os.system(command)
+
+    # Remove the original file
+    os.remove(video_path)
+
+    # Rename the watermarked file to the original filename
+    os.rename(output_path, video_path)
+
+
+# Handler function for video messages
+@app.on_message(video_filter)
+async def watermark_video(client: Client, message: Message):
+    # Download the video
+    video_path = await message.download(file_name="temp_video.mp4")
+
+    # Set the watermark on the video
+    set_watermark(video_path)
+
+    # Send the watermarked video
+    await message.reply_video(video_path)
+
+    # Remove the video file from the server
+    os.remove(video_path)
+
+
+# Run the client
 app.run()
