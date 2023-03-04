c86ef2a971ba8dd65e89e4f20327ef301c7b01e8
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-05 00:35:31 +0530

    Update filename.py

--- filename.py
+++ filename.py
@@ -1,39 +1,37 @@
-import os
 from pyrogram import Client, filters
-from moviepy.editor import *
-from moviepy.video.tools.subtitles import SubtitlesClip
-from moviepy.video.VideoClip import TextClip
+from pyrogram.types import Message
+import os
 
-# API credentials
-API_ID = os.environ.get("15849735")
-API_HASH = os.environ.get("b8105dc4c17419dfd4165ecf1d0bc100")
-BOT_TOKEN = os.environ.get("6145559264:AAEkUH_znhpaTdkbnndwP1Vy2ppv-C9Zf4o")
+# Create a Pyrogram client instance
+api_id = 15849735
+api_hash = '15849735'
+bot_token = '6145559264:AAEkUH_znhpaTdkbnndwP1Vy2ppv-C9Zf4o'
+app = Client('my_bot', api_id, api_hash, bot_token=bot_token)
 
-# Create the Pyrogram client
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# Define a command handler for the /start command
+@app.on_message(filters.command('start'))
+def start_command_handler(client, message):
+    # Send a welcome message
+    client.send_message(message.chat.id, 'Hi! I am a bot that can add text to the top right corner of a video. '
+                                         'Send me a video file and reply to it with the /set command to add text.')
 
+# Define a command handler for the /set command
+@app.on_message(filters.command('set') & filters.reply)
+def set_command_handler(client, message):
+    # Check if the replied message is a video
+    if message.reply_to_message.video:
+        # Download the video file
+        video = client.download_media(message.reply_to_message.video)
+        # Add the text to the top right corner of the video using ffmpeg
+        os.system(f'ffmpeg -i "{video}" -vf "drawtext=text=@OnlyFanstash:x=w-tw-10:y=10:fontsize=20:fontcolor=white:box=1:boxcolor=black@0.5" -c:a copy output.mp4')
+        # Upload the modified video
+        client.send_video(message.chat.id, 'output.mp4', supports_streaming=True)
+        # Delete the downloaded and modified video files
+        os.remove(video)
+        os.remove('output.mp4')
+    else:
+        # Send an error message if the replied message is not a video
+        client.send_message(message.chat.id, 'Please reply to a video with the /set command.')
 
-def add_watermark(video_file, output_file, watermark_text):
-    """Adds a watermark to a video file."""
-    video = VideoFileClip(video_file)
-
-    # Add watermark text to the video
-    txt_clip = TextClip(watermark_text, fontsize=30, color='white').set_position("bottom")
-    result = CompositeVideoClip([video, txt_clip])
-
-    # Save the watermarked video
-    result.write_videofile(output_file)
-
-
-@app.on_message(filters.video & filters.reply)
-def set_watermark(client, message):
-    """Sets the watermark for the bot."""
-    video_file = client.download_media(message.reply_to_message)
-    watermark_text = "@OnlyFanstash"
-    output_file = "watermarked_" + os.path.basename(video_file)
-    add_watermark(video_file, output_file, watermark_text)
-    message.reply_video(output_file)
-
-
-if __name__ == "__main__":
-    app.run()
+# Start the Pyrogram client
+app.run()
