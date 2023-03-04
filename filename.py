873a5e7e9331d157325ac080049bebf1f56c3d2f
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-05 00:18:34 +0530

    Update filename.py

--- filename.py
+++ filename.py
@@ -24,29 +24,25 @@ def add_watermark(video_path, watermark_path, output_path):
 # Define the message handler function
 @app.on_message(filters.video & ~filters.forwarded)
 def handle_message(client: Client, message: Message):
-    # Download the video file
-    video_file = client.download_media(
-        message=message.video,
-        file_name="video.mp4"
-    )
-
-    # Store the message ID to reference it later
-    watermark_message_id = message.message_id
-
-    # Ask the user to send the watermark image file
-    message.reply_text("Please reply to this message with the watermark image file.")
-
-    # Listen for the reply containing the watermark image file
-    @app.on_message(filters.reply & filters.photo)
-    def handle_watermark_message(client: Client, message: Message):
-        # Remove the listener so that it doesn't keep running unnecessarily
-        app.remove_handler(handle_watermark_message)
-
-        # Download the watermark image file
+    if message.text and message.text.startswith("/set"):
         watermark_file = client.download_media(
-            message=message.photo[-1],
+            message=message.reply_to_message,
             file_name="watermark.png"
         )
+        message.reply_text("Watermark set successfully!")
+    else:
+        # Download the video file
+        video_file = client.download_media(
+            message=message.video,
+            file_name="video.mp4"
+        )
+
+        # Check if a watermark has been set
+        if os.path.exists("watermark.png"):
+            watermark_file = "watermark.png"
+        else:
+            # If no watermark has been set, use a default watermark
+            watermark_file = "default_watermark.png"
 
         # Generate the output file path
         output_file = os.path.join(
@@ -57,27 +53,11 @@ def handle_message(client: Client, message: Message):
         # Add the watermark to the video
         add_watermark(video_file, watermark_file, output_file)
 
-        # Send the watermarked video as a reply to the original message
+        # Send the watermarked video as a reply
         message.reply_video(
             video=output_file,
             quote=True
         )
-
-    # Store the watermark message ID to reference it later
-    client.set_relay_data(watermark_message_id, "watermark_message_id")
-
-# Listen for the /set command
-@app.on_message(filters.command("set"))
-def handle_set_command(client: Client, message: Message):
-    # Get the watermark message ID from the original message
-    watermark_message_id = client.get_relay_data(message.reply_to_message.message_id, "watermark_message_id")
-
-    # If the watermark message ID exists, delete the watermark message
-    if watermark_message_id:
-        client.delete_messages(message.chat.id, watermark_message_id)
-        message.reply_text("Watermark image message deleted.")
-    else:
-        message.reply_text("No watermark image message found.")
 
 # Start the Pyrogram client
 app.run()
