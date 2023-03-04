72af3d36a80eebfbb300d23142dcd5f989f19b1d
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-05 00:24:02 +0530

    Update filename.py

--- filename.py
+++ filename.py
@@ -4,45 +4,55 @@ import os
 from moviepy.editor import *
 
 # Add your API ID, API hash, and bot token here
-api_id = 12345
-api_hash = "0123456789abcdef0123456789abcdef"
-bot_token = "1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+api_id = 15849735
+api_hash = "b8105dc4c17419dfd4165ecf1d0bc100"
+bot_token = "6145559264:AAEkUH_znhpaTdkbnndwP1Vy2ppv-C9Zf4o"
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
+# Define watermark image path
+watermark_file = None
 
 # Define the watermark function
-def add_watermark(video_path, watermark_path, output_path):
+def add_watermark(video_path, output_path):
     video = VideoFileClip(video_path)
-    watermark = (ImageClip(watermark_path)
+    watermark = (ImageClip(watermark_file)
                  .resize(height=video.h / 6) # You can adjust the size of the watermark here
                  .margin(right=8, top=8, opacity=0) # You can adjust the position of the watermark here
                  .set_pos(("right", "top")))
     result = CompositeVideoClip([video, watermark])
     result.write_videofile(output_path)
 
-# Define the message handler function
-@app.on_message(filters.video & ~filters.forwarded)
-def handle_message(client: Client, message: Message):
-    if message.text and message.text.startswith("/set"):
+# Define the /start command handler
+@app.on_message(filters.command("start"))
+def start(client: Client, message: Message):
+    message.reply_text("Hi! I can add a watermark to your videos. To get started, reply to a video with /set and upload a watermark image.")
+
+# Define the /set command handler
+@app.on_message(filters.command("set"))
+def set_watermark(client: Client, message: Message):
+    # Check if the message is a reply to a video
+    if message.reply_to_message and message.reply_to_message.video:
+        # Download the watermark image file
+        global watermark_file
         watermark_file = client.download_media(
-            message=message.reply_to_message,
+            message=message,
             file_name="watermark.png"
         )
-        message.reply_text("Watermark set successfully!")
+        message.reply_text("Watermark image set successfully! You can now reply to a video with /watermark to get the watermarked video.")
     else:
+        message.reply_text("Please reply to a video to set the watermark image.")
+
+# Define the /watermark command handler
+@app.on_message(filters.command("watermark"))
+def watermark(client: Client, message: Message):
+    # Check if the message is a reply to a video
+    if message.reply_to_message and message.reply_to_message.video:
         # Download the video file
         video_file = client.download_media(
-            message=message.video,
+            message=message.reply_to_message,
             file_name="video.mp4"
         )
-
-        # Check if a watermark has been set
-        if os.path.exists("watermark.png"):
-            watermark_file = "watermark.png"
-        else:
-            # If no watermark has been set, use a default watermark
-            watermark_file = "default_watermark.png"
 
         # Generate the output file path
         output_file = os.path.join(
@@ -51,13 +61,15 @@ def handle_message(client: Client, message: Message):
         )
 
         # Add the watermark to the video
-        add_watermark(video_file, watermark_file, output_file)
+        add_watermark(video_file, output_file)
 
         # Send the watermarked video as a reply
         message.reply_video(
             video=output_file,
             quote=True
         )
+    else:
+        message.reply_text("Please reply to a video with /watermark to get the watermarked video.")
 
 # Start the Pyrogram client
 app.run()
