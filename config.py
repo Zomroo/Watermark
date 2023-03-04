000fd5974bbb321a2d1bb18efb490c74f4eb8630
Zomroo <93994060+Zomroo@users.noreply.github.com>
2023-03-04 23:18:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+    # Replace with your Telegram API ID
+    API_ID = 15849735
+
+    # Replace with your Telegram API hash
+    API_HASH = "b8105dc4c17419dfd4165ecf1d0bc100"
+
+    # Replace with your bot token
+    BOT_TOKEN = "6067171502:AAF20GwzoxblYC4yBNw8nTffRJprwgLTKi0"
+
+    # Replace with your Streamable account credentials
+    STREAMABLE_USERNAME = "my_streamable_username"
+    STREAMABLE_PASSWORD = "my_streamable_password"
